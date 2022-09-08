a2a5c3b22d17a61ea70dbc4904aedd1b4fa35116
olegpl1993 <olegpl1993@gmail.com>
2022-09-08 21:12:05 +0300

    feat: cteate template project

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,61 @@
 import './styles/index.scss';
-import five from './ts/five';
-import People from './ts/people';
 
-const hi = 'Hell world ';
+// верстка приложения ---------------------------------------------------------
+const wrapper:HTMLDivElement = document.createElement('div');
+wrapper.classList.add('wrapper');
+document.body.appendChild(wrapper);
 
-const txt = 'some text';
-console.log(txt);
+const upperText:HTMLDivElement = document.createElement('div');
+upperText.classList.add('upperText');
+upperText.textContent = 'proj News';
+wrapper.appendChild(upperText);
 
-function plus (a:string, b:number) {
-    return a + b;
-}
-console.log(plus(hi, five));
+const section:HTMLDivElement = document.createElement('div');
+section.classList.add('section');
+wrapper.appendChild(section);
 
-const oleg:People = new People('Oleg', 28);
-const lena:People = new People('Lena', 33);
+const menuBox:HTMLDivElement = document.createElement('div');
+menuBox.classList.add('menuBox');
+section.appendChild(menuBox);
 
-oleg.setPass('abra357951');
+const contentBox:HTMLDivElement = document.createElement('div');
+contentBox.classList.add('contentBox');
+section.appendChild(contentBox);
 
-lena.setPass('kity159357')
-lena.sayName();
-lena.sayAge();
-lena.sayPass();
+//----------------------------------------------------------------------------
 
-console.log(oleg);
-console.log(lena);
+// источники новостей---------------------------------------------------------
+const source:HTMLDivElement = document.createElement('div');
+source.classList.add('source');
+source.textContent = 'source News';
+menuBox.appendChild(source);
+
+const source1:HTMLDivElement = document.createElement('div');
+source1.classList.add('source');
+source1.textContent = 'source News 1';
+menuBox.appendChild(source1);
+
+const source2:HTMLDivElement = document.createElement('div');
+source2.classList.add('source');
+source2.textContent = 'source News 2';
+menuBox.appendChild(source2);
+
+//-----------------------------------------------------------------------------
+
+// блоки новостей -------------------------------------------------------------
+const newsBox:HTMLDivElement = document.createElement('div');
+newsBox.classList.add('newsBox');
+newsBox.textContent = 'news Box';
+contentBox.appendChild(newsBox);
+
+const newsBox1:HTMLDivElement = document.createElement('div');
+newsBox1.classList.add('newsBox');
+newsBox1.textContent = 'news Box 1';
+contentBox.appendChild(newsBox1);
+
+const newsBox2:HTMLDivElement = document.createElement('div');
+newsBox2.classList.add('newsBox');
+newsBox2.textContent = 'news Box 2';
+contentBox.appendChild(newsBox2);
+
+//-----------------------------------------------------------------------------
